07fbb104e61611bb506d7ba3fca66cb7b448fd54
Meg Scholl <meghan.e.scholl@gmail.com>
2020-01-22 10:19:51 -0600

    edited blog css margins and added blog png

--- src/Icons/Icons.js
+++ src/Icons/Icons.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Design from '../images/my_design.png';
 import Dev from '../images/my_dev.png';
-import Ux from '../images/ux_2.png';
+// import Ux from '../images/ux_2.png';
+import Blog from '../images/blog.png'
 import './Icons.css';
 
 function Icons() {
@@ -13,8 +14,11 @@ function Icons() {
             <a href="." alt="" title="" className="icon-hover">
                 <img src={Dev} className="icon-logo" alt="" />
             </a>
-            <a href="." alt="" title="" className="icon-hover">
+            {/* <a href="." alt="" title="" className="icon-hover">
                 <img src={Ux} className="icon-logo" alt="" />
+            </a> */}
+            <a href="." alt="" title="" className="icon-hover">
+                <img src={Blog} className="icon-logo" alt="" />
             </a>
         </div>
     );
