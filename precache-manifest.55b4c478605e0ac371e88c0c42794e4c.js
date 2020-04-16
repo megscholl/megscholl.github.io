0c5ec36f3ba075d12819b2f817d4e353878e97f3
Meg Scholl <meghan.e.scholl@gmail.com>
2020-04-16 10:16:06 -0500

    service-worker

---
+++ precache-manifest.55b4c478605e0ac371e88c0c42794e4c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a76c515be27a992581bcd8e9b241c64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39616285df82dfe364a3",
+    "url": "/static/css/2.0b76d70b.chunk.css"
+  },
+  {
+    "revision": "99330c035e429204d5b0",
+    "url": "/static/css/main.d0ba61eb.chunk.css"
+  },
+  {
+    "revision": "39616285df82dfe364a3",
+    "url": "/static/js/2.f61469f2.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.f61469f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99330c035e429204d5b0",
+    "url": "/static/js/main.bb5ea65d.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "b608a61b93ef523c8884d82692590003",
+    "url": "/static/media/blog.b608a61b.png"
+  },
+  {
+    "revision": "a32c4e4654a9b139d29dfb688480ba0e",
+    "url": "/static/media/floral3.a32c4e46.png"
+  },
+  {
+    "revision": "d2f2d0d73387267d11999f9284852679",
+    "url": "/static/media/floral404.d2f2d0d7.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "70ebb2800b8d52fe97b7fd4248283dd3",
+    "url": "/static/media/me.70ebb280.png"
+  },
+  {
+    "revision": "bdf318f75afb7ab13b16f8aa4149a325",
+    "url": "/static/media/my_design.bdf318f7.png"
+  },
+  {
+    "revision": "fcbe6a1a3b4de95fbaf1c5f7a58515cf",
+    "url": "/static/media/my_dev.fcbe6a1a.png"
+  },
+  {
+    "revision": "fd2413a4911f24893c2a9faf220b359b",
+    "url": "/static/media/under_construction.fd2413a4.png"
+  }
+]);
